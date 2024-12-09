18a59c1f6a9ff27d43e0aa78439d086d6b17a5b5
czerwinskicez <czerwinskicez@gmail.com>
2024-12-09 19:27:26 +0100

    beta test

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-app.get('/', (req, res) => res.send({"Hello": "gamma!", "CI/CD": true, }, ));
+app.get('/', (req, res) => res.send({"Hello": "beta!", "CI/CD": true, }, ));
 
 app.listen(port, () => console.log(`App running at http://localhost:${port}`));
