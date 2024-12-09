f2835dedc44aa0260bcd6afcfab91e9933f98800
czerwinskicez <czerwinskicez@gmail.com>
2024-12-09 19:10:18 +0100

    hello gamma test

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-app.get('/', (req, res) => res.send({"Hello": "world!"}));
+app.get('/', (req, res) => res.send({"Hello": "gamma!"}));
 
 app.listen(port, () => console.log(`App running at http://localhost:${port}`));
