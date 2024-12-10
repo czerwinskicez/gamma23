7cc8e92740d3ffa976995ce58c58ad0895fc44a6
czerwinskicez <czerwinskicez@gmail.com>
2024-12-10 18:09:29 +0100

    implemented login frontend

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 4000;
 
-app.get('/', (req, res) => res.send({"Hello": "gamma!", "CI/CD": true, }, ));
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/bootstrap", express.static(path.join(__dirname, "node_modules/bootstrap")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+    return res.redirect("/login");
+});
+app.get('/login', (req, res) => {
+    return res.render("index");
+});
 
 app.listen(port, () => console.log(`App running at http://localhost:${port}`));
+
