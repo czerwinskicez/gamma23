d3eb785a8a88e11ac2b28d573b2e505e88ccd062
czerwinskicez <czerwinskicez@gmail.com>
2025-01-09 12:43:17 +0100

    r290test5

--- public/external/r290vsr32custom.js
+++ public/external/r290vsr32custom.js
@@ -1,13 +1,28 @@
 document.addEventListener("DOMContentLoaded", _=>{
-    console.log(document.querySelectorAll(".collapsible"));
-});
+    const collapsibles = document.querySelectorAll(".collapsible");
 
-if(location.pathname=='/glowna_preprod'){
-    // load custom css
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = "https://gamma.23.net.pl/public/external/r290vsr32custom.css";
-    link.type = "text/css";
-    link.media = "all";
-    document.head.appendChild(link);
-}
+    collapsibles.forEach(collapsible=>{
+        collapsible.querySelectorAll("label.toggle-label").forEach(toggleLabel=>{
+            // toggleLabel.style.display = "flex";
+            toggleLabel.classList.add("custom-toggle-label");
+            
+            const zobaczButton = document.createElement("span.zobacz-button");
+            zobaczButton.innerText = "Zobacz";
+            // zobaczButton.style.display = "block";
+            // zobaczButton.style.marginLeft = "auto";
+
+            toggleLabel.appendChild(zobaczButton);
+        });
+    });
+
+    if(location.pathname=='/glowna_preprod'){
+        // load custom css
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = "https://gamma.23.net.pl/public/external/r290vsr32custom.css";
+        link.type = "text/css";
+        link.media = "all";
+        document.head.appendChild(link);
+    }
+
+});
