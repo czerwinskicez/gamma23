50aeb56d3123380c11ce73455836779d6186c709
czerwinskicez <czerwinskicez@gmail.com>
2025-01-09 12:25:39 +0100

    r290test3

--- public/external/r290vsr32custom.js
+++ public/external/r290vsr32custom.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", _=>{
     console.log(document.querySelectorAll(".collapsible"));
 });
+
+if(location.pathname=='/glowna_preprod'){
+    // load custom css
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "r290vsr32custom.css";
+    link.type = "text/css";
+    link.media = "all";
+    document.head.appendChild(link);
+}
